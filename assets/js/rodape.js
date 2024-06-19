93c8dec45809b9d4b0c4b3108549760c00398ab5
ymbitancourt <115833606+ymbitancourt@users.noreply.github.com>
2024-06-19 00:44:36 -0300

    assets

---
+++ assets/js/rodape.js
@@ -0,0 +1,17 @@
+//---- Troca de cor do footer conforme página ----\\
+jQuery("document").ready(function ($) {
+  var pg_number = parseInt(window.location.href.split('?')[1].split('=')[1]);
+  if (pg_number == 1) {
+    $("footer").addClass("footer1");
+  } else if (pg_number == 2) {
+    $("footer").addClass("footer2");
+  } else if (pg_number == 3) {
+    $("footer").addClass("footer3");
+  } else if (pg_number == 4) {
+    $("footer").addClass("footer4");
+  } else if (pg_number == 5) {
+    $("footer").addClass("footer5");
+  } else if (pg_number == 6) {
+    $("footer").addClass("footer6");
+  }
+});
